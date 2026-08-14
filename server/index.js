b3b41299e5855b412b320e1db266b572bbb0e83f
TusharNagpal-1 <nagpaltushar669@gmail.com>
2026-08-14 12:53:28 +0530

    port modified

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
